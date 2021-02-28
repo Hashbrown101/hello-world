8cef7c72c481f7db64b8cd9b68b5409b36a5f32a
Hashbrown101 <42585614+Hashbrown101@users.noreply.github.com>
2021-02-28 16:26:05 -0700

    Revert "Swapping code"

--- GitDemo.java
+++ GitDemo.java
@@ -18,12 +18,7 @@ public class GitDemo {
         previousNumber += num;
         return 0;
     }
-     /*****
-     * This function should reverse 'num' and return the resulting value.
-     * i.e. if num = 1234
-     *  the function will return 4321
-     *****/
-    public double reverseNumber(int num){
+    
     /*****
      * This function should calculate the factorial of 'num' and return the resulting value.
      * i.e. if num = 4,
@@ -42,11 +37,11 @@ public class GitDemo {
     }
     
     /*****
-     * This function should calculate the factorial of 'num' and return the resulting value.
-     * i.e. if num = 4,
-     *  the function will return the value of (1 * 2 * 3 * 4)
+     * This function should reverse 'num' and return the resulting value.
+     * i.e. if num = 1234
+     *  the function will return 4321
      *****/
-    public double findFactorial(int num) {
+    public double reverseNumber(int num){
         double newNumber = 1;
         
         for(int i = 1; i <= num; ++i){
